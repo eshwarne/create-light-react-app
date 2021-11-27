c861cb68249c9a431253597922f0957969196e6f
Eshwar S <eshwars@Eshwars-MacBook-Pro.local>
2021-11-27 14:18:07 +0530

    Add initial react component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1>Welcome to <span style={{color:"blue"}}>LIGHT REACT</span></h1>
+        </div>
+    )
+}
+
+export default HelloWorld
